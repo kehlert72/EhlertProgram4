2236a6a2ed0426c99266343259e4bd78792a37e5
kehlert72 <kaleb.ehlert@gmail.com>
2020-07-27 17:00:28 -0400

    Initial commit

---
+++ EhlertProgram4/T-Shirt_Order_Form.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace EhlertProgram4
+{
+    partial class frmTshirtOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuProcess = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuItemDisplayOrder = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ckLstSize = new System.Windows.Forms.CheckedListBox();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.cboQuantity = new System.Windows.Forms.ComboBox();
+            this.lstInfo = new System.Windows.Forms.ListBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.lblOrderInfo = new System.Windows.Forms.Label();
+            this.txtOrderInfo = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuProcess});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1151, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuProcess
+            // 
+            this.mnuProcess.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuItemDisplayOrder,
+            this.toolStripSeparator1,
+            this.mnuItemExit});
+            this.mnuProcess.Name = "mnuProcess";
+            this.mnuProcess.Size = new System.Drawing.Size(59, 20);
+            this.mnuProcess.Text = "Process";
+            // 
+            // mnuItemDisplayOrder
+            // 
+            this.mnuItemDisplayOrder.Name = "mnuItemDisplayOrder";
+            this.mnuItemDisplayOrder.Size = new System.Drawing.Size(152, 22);
+            this.mnuItemDisplayOrder.Text = "Display Order";
+            this.mnuItemDisplayOrder.Click += new System.EventHandler(this.displayOrderToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // mnuItemExit
+            // 
+            this.mnuItemExit.Name = "mnuItemExit";
+            this.mnuItemExit.Size = new System.Drawing.Size(152, 22);
+            this.mnuItemExit.Text = "Exit";
+            this.mnuItemExit.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // ckLstSize
+            // 
+            this.ckLstSize.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ckLstSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckLstSize.FormattingEnabled = true;
+            this.ckLstSize.Items.AddRange(new object[] {
+            "Small",
+            "Medium",
+            "Large",
+            "X-Large"});
+            this.ckLstSize.Location = new System.Drawing.Point(49, 249);
+            this.ckLstSize.Name = "ckLstSize";
+            this.ckLstSize.Size = new System.Drawing.Size(381, 112);
+            this.ckLstSize.TabIndex = 3;
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(62, 221);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(54, 25);
+            this.lblSize.TabIndex = 4;
+            this.lblSize.Text = "Size";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(62, 380);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(98, 25);
+            this.lblQuantity.TabIndex = 5;
+            this.lblQuantity.Text = "Quantity:";
+            // 
+            // cboQuantity
+            // 
+            this.cboQuantity.FormattingEnabled = true;
+            this.cboQuantity.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cboQuantity.Location = new System.Drawing.Point(287, 380);
+            this.cboQuantity.Name = "cboQuantity";
+            this.cboQuantity.Size = new System.Drawing.Size(121, 33);
+            this.cboQuantity.TabIndex = 6;
+            // 
+            // lstInfo
+            // 
+            this.lstInfo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lstInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstInfo.FormattingEnabled = true;
+            this.lstInfo.ItemHeight = 25;
+            this.lstInfo.Items.AddRange(new object[] {
+            "Name:",
+            "",
+            "Phone:",
+            "",
+            "Email:"});
+            this.lstInfo.Location = new System.Drawing.Point(49, 65);
+            this.lstInfo.Name = "lstInfo";
+            this.lstInfo.Size = new System.Drawing.Size(381, 129);
+            this.lstInfo.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(287, 65);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 29);
+            this.txtName.TabIndex = 8;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(287, 163);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 29);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.Location = new System.Drawing.Point(287, 115);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 29);
+            this.txtPhone.TabIndex = 10;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(62, 37);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(104, 25);
+            this.lblInfo.TabIndex = 11;
+            this.lblInfo.Text = "Customer";
+            // 
+            // lblOrderInfo
+            // 
+            this.lblOrderInfo.AutoSize = true;
+            this.lblOrderInfo.Location = new System.Drawing.Point(62, 428);
+            this.lblOrderInfo.Name = "lblOrderInfo";
+            this.lblOrderInfo.Size = new System.Drawing.Size(101, 25);
+            this.lblOrderInfo.TabIndex = 12;
+            this.lblOrderInfo.Text = "OrderInfo";
+            // 
+            // txtOrderInfo
+            // 
+            this.txtOrderInfo.Location = new System.Drawing.Point(49, 456);
+            this.txtOrderInfo.Multiline = true;
+            this.txtOrderInfo.Name = "txtOrderInfo";
+            this.txtOrderInfo.Size = new System.Drawing.Size(381, 151);
+            this.txtOrderInfo.TabIndex = 13;
+            // 
+            // frmTshirtOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1151, 619);
+            this.Controls.Add(this.txtOrderInfo);
+            this.Controls.Add(this.lblOrderInfo);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lstInfo);
+            this.Controls.Add(this.cboQuantity);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.ckLstSize);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "frmTshirtOrder";
+            this.Text = "T-Shirt Order Form";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuProcess;
+        private System.Windows.Forms.ToolStripMenuItem mnuItemDisplayOrder;
+        private System.Windows.Forms.ToolStripMenuItem mnuItemExit;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.CheckedListBox ckLstSize;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.ComboBox cboQuantity;
+        private System.Windows.Forms.ListBox lstInfo;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label lblOrderInfo;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.TextBox txtOrderInfo;
+    }
+}
+
---
+++ EhlertProgram4/T-Shirt_Order_Form.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+/*  ID
+ *      T-Shirt Order Form, Kaleb Ehlert, 2/4/2018
+ *  Purpose
+ *      The purpose of the program is to take the info of the customer along with the size and quantity of the shirt the user wants and output the info line-by-line.
+ *  Form Controls
+ *      Submission: lblInfo: Labels the listbox asking for info on the customer,    lstInfo: Labels the info wanted by the customer,    txtName: Captures the name of the
+ *      user in the textbox,    txtEmail: Captures the email of the user in the textbox,    txtPhone: Captures the phone number of the user in the textbox,
+ *      lblSize: Labels the check list size,   lstSize: Allows for the user to pick a size for the t-shirt, lblQuantity: Labels the quantity combo box, cboQuantity:
+ *      Allows the user to pick a desired quantity, lblOrderInfo: Labels the text box OrderInfo,    txtOrderInfo: Displays all of the information the user has entered,
+ *      mnuItemDisplayOrder: When clicked the menu item will process the order and output the input that was processed, mnuItemExit: A menu item that will exit the program.
+ *  Code & Logic
+ *      The name, phone number, email, size, and quantity is needed for input from the user.
+ *      The processing is taking the input from the user and setting it in a string. Each input is set on it's own line.
+ *      The output is the multiline text box getting the string and display that string. The result is the information being displayed line-by-line.
+ */
+
+namespace EhlertProgram4
+{
+    public partial class frmTshirtOrder : Form
+    {
+        public frmTshirtOrder()
+        {
+            InitializeComponent();
+        }
+
+        // The event for DisplayOrder when triggered will collect all data the user has given and display that information.
+        //This is the method it calls on.
+        private void displayOrderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Empty the text from the order info
+            txtOrderInfo.Text = "";
+
+            // Get the name, phone number, and email
+            txtOrderInfo.Text += txtName.Text + "\r\n" + txtPhone.Text + "\r\n" + txtEmail.Text + "\r\n";
+
+            // Get the size
+            txtOrderInfo.Text += ckLstSize.Text + "\r\n";
+
+            // Get the quantity
+            txtOrderInfo.Text += cboQuantity.Text + "\r\n";
+        }
+
+        // The event for Exit when triggered will end the program.
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
